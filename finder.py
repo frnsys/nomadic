46e0bc2baf5c6325f438851cd9ef71f79138e17a
Francis Tseng <f+accounts@frnsys.com>
2014-09-19 10:49:03 -0400

    clean up

--- finder.py
+++
@@ -1,8 +0,0 @@
-import re
-from nomadic import nomadic
-import click
-
-for note in nomadic.rootbook.notes:
-    if not note.content:
-        print(note.path.abs)
-        #click.edit(filename=note.path.abs)
