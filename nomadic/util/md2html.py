9f2c7900bf2ce7725bd08004c4e7795a8f06d08e
Francis Tseng <f+accounts@frnsys.com>
2014-10-06 02:25:32 -0400

    mathjax handling includes inline mathjax as well, denoted :: ::

--- nomadic/util/md2html.py
+++ nomadic/util/md2html.py
@@ -59,11 +59,12 @@ class MathJaxHandler():
     There might be a better way of handling MathJax buuuut this will do for now.
     """
     MATHJAX_RE = re.compile(r'\${2}.+?\${2}', re.DOTALL)
+    MATHJAX_INLINE_RE = re.compile(r'\:{2}.+?\:{2}', re.DOTALL)
     PLACEHOLDER = '<MATHJAXHOLDER>'
 
     def extract(self, doc):
         self.formulae = []
-        for match in self.MATHJAX_RE.findall(doc):
+        for match in self.MATHJAX_RE.findall(doc) + self.MATHJAX_INLINE_RE.findall(doc):
             self.formulae.append(match)
             doc = doc.replace(match, self.PLACEHOLDER)
         return doc
