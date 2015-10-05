4db1056c61f9a3085351e6c6417b5b17a21b0a82
Francis Tseng <f+accounts@frnsys.com>
2015-10-05 12:52:27 -0400

    ignore dirs starting with '.' or '_' as notebooks

--- nomadic/util/__init__.py
+++ nomadic/util/__init__.py
@@ -7,6 +7,11 @@ def valid_notebook(path):
     """
     if not os.path.isdir(path):
         return False
+
+    # Ignore hidden directories and directories starting with '_'
+    if path.strip('/').split('/')[-1][0] in ['.', '_']:
+        return False
+
     for excluded in ['.searchindex', '_resources', 'assets', '.SyncArchive', '.SyncID', '.SyncIgnore', '.sync', '.DS_Store', '.swp', '.swo', '.stfolder', '.git']:
         if excluded in path: return False
     return True
